765dc57aca8a9a1a55289a11f732c2eac5b8aaa6
Hayk Gevorkyan <gevorkyan.ayk1904@gmail.com>
2025-05-25 19:27:26 +0300

    bug fixes 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,16 +1,15 @@
 import os
 from pathlib import Path
 
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)'
-    'abgjw8mo!4y-q*uq1!4$-89$'
-)
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['158.160.26.249', 'kittygram-online.zapto.org']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
