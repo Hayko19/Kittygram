375adfdf041d9ac8975f51492238b0db2a55db19
Hayk Gevorkyan <gevorkyan.ayk1904@gmail.com>
2025-05-25 19:39:55 +0300

    bug fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='default-secret-key')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
